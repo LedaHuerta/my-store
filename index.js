df97909e82c200158e136e7537f16a2364d757d0
Leda Michelle Huerta Recinas <leda.huerta@interware.local>
2022-02-14 15:06:35 -0600

    Instalación de Express.js y tu primer servidor HTTP

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("My app");
+const express = require('express');
+const app = express();
+const port = 3005;
+
+app.get('/', (req, res) => {
+  res.send('Hello World, this is my first server on express');
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+  console.log('My port: ', port);
+});
