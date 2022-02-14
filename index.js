99aa945617e74c4cc6bb60930dd5ce1f77784409
Leda Michelle Huerta Recinas <leda.huerta@interware.local>
2022-02-14 15:23:31 -0600

    agregando ipv4

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
 const port = 3005;
+const ip = '192.168.137.1';
 
 app.get('/', (req, res) => {
   res.send('Hello World, this is my first server on express');
 });
 
 app.listen(port, () => {
+  // console.log('My port: ', port);
   console.log(`Listening on port ${port}`);
-  console.log('My port: ', port);
+  console.log(`http://${ip}:${port}/`);
 });
