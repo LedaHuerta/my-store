e1d244f6f8e22f438eb712317e0d89ede5dda265
Leda Michelle Huerta Recinas <leda.huerta@interware.local>
2022-02-14 22:50:39 -0600

    transformando metodos :a sync

--- routes/products.router.js
+++ routes/products.router.js
@@ -4,8 +4,8 @@ const router = express.Router();
 
 const service = new ProductsService();
 
-router.get('/', (req, res) => {
-  const products = service.find();
+router.get('/', async (req, res) => {
+  const products = await service.find();
   res.json(products);
 });
 
@@ -13,39 +13,39 @@ router.get('/filter', (req, res) => {
   res.send('Yo soy un filter');
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   const { id } = req.params;
-  const product = service.findOne(id);
+  const product = await service.findOne(id);
   res.json(product);
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   const body = req.body;
-  const newProduct = service.create(body);
+  const newProduct = await service.create(body);
   res.status(201).json(newProduct);
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   const {
     body,
     params: { id },
   } = req;
-  const product = service.update(id, body);
+  const product = await service.update(id, body);
   res.json(product);
 });
 
-router.patch('/:id', (req, res) => {
+router.patch('/:id', async (req, res) => {
   const {
     body,
     params: { id },
   } = req;
-  const product = service.patch(id, body);
+  const product = await service.patch(id, body);
   res.json(product);
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   const { id } = req.params;
-  const rta = service.delete(id);
+  const rta = await service.delete(id);
   res.json(rta);
 });
 
--- services/product.service.js
+++ services/product.service.js
@@ -20,7 +20,7 @@ class ProductsService {
     }
   }
 
-  create(data) {
+  async create(data) {
     const newProduct = {
       id: casual.uuid,
       ...data,
@@ -30,14 +30,18 @@ class ProductsService {
   }
 
   find() {
-    return this.products;
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(this.products);
+      }, 5000);
+    });
   }
 
-  findOne(id) {
+  async findOne(id) {
     return this.products.find((item) => item.id === id);
   }
 
-  update(id, changes) {
+  async update(id, changes) {
     const index = this.products.findIndex((item) => item.id === id);
     if (index === -1) {
       throw new Error('Product Not Found');
@@ -46,7 +50,7 @@ class ProductsService {
     return this.products[index];
   }
 
-  patch(id, changes) {
+  async patch(id, changes) {
     const index = this.products.findIndex((item) => item.id === id);
     if (index === -1) {
       throw new Error('Product Not Found');
@@ -59,7 +63,7 @@ class ProductsService {
     return this.products[index];
   }
 
-  delete(id) {
+  async delete(id) {
     const index = this.products.findIndex((item) => item.id === id);
     if (index === -1) {
       throw new Error('Product Not Found');
