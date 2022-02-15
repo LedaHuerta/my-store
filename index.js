26ed0a0e199a129af13ac20cb60a636f7be95aab
Leda Michelle Huerta Recinas <leda.huerta@interware.local>
2022-02-14 18:55:56 -0600

    división de responabilidades

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const casual = require('casual');
+const routerApi = require('./routes');
 const app = express();
 const port = 3005;
 const ip = '192.168.137.1';
@@ -12,29 +12,9 @@ app.get('/home', (req, res) => {
   res.send('Bienvenidos a My Shop');
 });
 
-app.get('/users', (req, res) => {
-  const { limit, offset } = req.query;
-  if (limit && offset) {
-    res.json({
-      limit,
-      offset,
-    });
-  } else {
-    res.send('No hay parámetros');
-  }
-});
-
-app.get('/categories/:categoryId/products/:productId', (req, res) => {
-  const { categoryId, productId } = req.params;
-  res.json({
-    categoryId,
-    productId,
-    name: 'Product 2',
-    price: 2000,
-  });
-});
-
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
   console.log(`http://${ip}:${port}/`);
 });
+
+routerApi(app);
