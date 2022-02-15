9133f3ac3ca436c432ea21f5fc334ac634f4431a
Leda Michelle Huerta Recinas <leda.huerta@interware.local>
2022-02-15 03:33:06 -0600

    CORS

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 const express = require('express');
+const cors = require('cors');
 const routerApi = require('./routes');
 const {
   logErrors,
   errorHandler,
   boomErrorHandler,
 } = require('./middlewares/error.handler');
+
 const app = express();
 const port = 3005;
 const ip = '192.168.137.1';
 
 app.use(express.json());
+const whitelist = ['http://127.0.0.1:5500', 'http://localhost:5500'];
+const options = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('No tiene permiso para acceder'));
+    }
+  },
+};
+app.use(cors(options));
 
 app.get('/', (req, res) => {
   res.send('Hello World, this is my first server on express');
@@ -19,15 +32,15 @@ app.get('/home', (req, res) => {
   res.send('Bienvenidos a My Shop');
 });
 
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
-  console.log(`http://${ip}:${port}/api/v1`);
-  console.log(`http://localhost:${port}/api/v1`);
-});
-
 routerApi(app);
 //Los middleware de tipo error se deben poner después de definir el routing
 //En el orden en que se pongan aquí, es como se van a ejecutar.
 app.use(logErrors);
 app.use(boomErrorHandler);
 app.use(errorHandler);
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+  console.log(`http://${ip}:${port}/api/v1`);
+  console.log(`http://localhost:${port}/api/v1`);
+});
--- routes/products.router.js
+++ routes/products.router.js
@@ -1,5 +1,13 @@
 const express = require('express');
 const ProductsService = require('../services/product.service');
+const validatorHandler = require('../middlewares/validator.handler');
+const {
+  createProductSchema,
+  updateProductSchema,
+  patchProductSchema,
+  getProductSchema,
+  deleteProductSchema,
+} = require('../schemas/product.schema');
 const router = express.Router();
 
 const service = new ProductsService();
@@ -17,56 +25,78 @@ router.get('/filter', (req, res) => {
   res.send('Yo soy un filter');
 });
 
-router.get('/:id', async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const product = await service.findOne(id);
-    res.status(200).json(product);
-  } catch (err) {
-    next(err);
+router.get(
+  '/:id',
+  validatorHandler(getProductSchema, 'params'),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const product = await service.findOne(id);
+      res.status(200).json(product);
+    } catch (err) {
+      next(err);
+    }
   }
-});
+);
 
-router.post('/', async (req, res) => {
-  const body = req.body;
-  const newProduct = await service.create(body);
-  res.status(201).json(newProduct);
-});
-
-router.put('/:id', async (req, res, next) => {
-  try {
-    const {
-      body,
-      params: { id },
-    } = req;
-    const product = await service.update(id, body);
-    res.json(product);
-  } catch (err) {
-    next(err);
+router.post(
+  '/',
+  validatorHandler(createProductSchema, 'body'),
+  async (req, res) => {
+    const body = req.body;
+    const newProduct = await service.create(body);
+    res.status(201).json(newProduct);
   }
-});
+);
 
-router.patch('/:id', async (req, res, next) => {
-  try {
-    const {
-      body,
-      params: { id },
-    } = req;
-    const product = await service.patch(id, body);
-    res.json(product);
-  } catch (err) {
-    next(err);
+router.put(
+  '/:id',
+  validatorHandler(getProductSchema, 'params'), //primero valida el id
+  validatorHandler(updateProductSchema, 'body'), //luego valida el bod
+  async (req, res, next) => {
+    try {
+      const {
+        body,
+        params: { id },
+      } = req;
+      const product = await service.update(id, body);
+      res.json(product);
+    } catch (err) {
+      next(err);
+    }
   }
-});
+);
 
-router.delete('/:id', async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const rta = await service.delete(id);
-    res.json(rta);
-  } catch (err) {
-    next(err);
+router.patch(
+  '/:id',
+  validatorHandler(getProductSchema, 'params'), //primero valida el id
+  validatorHandler(patchProductSchema, 'body'), //luego valida el bod
+  async (req, res, next) => {
+    try {
+      const {
+        body,
+        params: { id },
+      } = req;
+      const product = await service.patch(id, body);
+      res.json(product);
+    } catch (err) {
+      next(err);
+    }
   }
-});
+);
+
+router.delete(
+  '/:id',
+  validatorHandler(deleteProductSchema, 'params'), //primero valida el id
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const rta = await service.delete(id);
+      res.json(rta);
+    } catch (err) {
+      next(err);
+    }
+  }
+);
 
 module.exports = router;
